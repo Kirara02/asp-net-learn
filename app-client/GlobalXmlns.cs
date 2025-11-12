03091ce576ecf136acd19de989f5465d22e983c5
Fathul Hidayat <fahtul.singaparna@gmail.com>
2025-11-12 16:37:33 +0700

    add initial app-client

---
+++ app-client/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "app_client")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "app_client.Pages")]
